fbc0dac0333b25d1998268ab2ba98b90b74d65a5
Sami <samishelbaya@proton.me>
2025-03-27 02:04:20 +0200

    fixed error handling

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import express from "express";
+import "express-async-errors";
 import userRouter from "./routes/users.js";
 import TaskRouter from "./routes/tasks.js";
 const app = express();
@@ -6,9 +7,7 @@ const port = 3001;
 app.use(express.json());
 app.use("/users", userRouter);
 app.use("/tasks", TaskRouter);
-app.use((err, req, res, next) => {
-  console.log("sex");
-  res.status(500).send("Something broke!");
-});
-
+app.use((err,req,res,next)=>{
+  res.status(500).send("Error")
+})
 app.listen(port, () => console.log(`Server Started on port ${port}`));
--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,19 +1,19 @@
 import { getAllUsers, getUserById, insertUser } from "../models/userModel.js";
 
 export async function fetchAllUsers(req, res) {
-  try {
-    const users = await getAllUsers();
-    res.status(200).json(users);
-  } catch (err) {
-    res.status(500).json({ error: "Failed to retrieve users" });
-  }
+  // try {
+  //   const users = await getAllUsers();
+  //   res.status(200).json(users);
+  // } catch (err) {
+  //   res.status(500).json({ error: "Failed to retrieve users" });
+  // }
+  throw new Error("Sex")
 }
 
-export async function fetchUser(req, res,next) {
+export async function fetchUser(req, res) {
   const id = req.params.id;
   const user = await getUserById(id);
   res.status(200).json(user);
-  // throw new Error("sex");
 }
 
 export async function CreateUser(req, res) {
